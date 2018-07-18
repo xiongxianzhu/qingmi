49eb3144d7da571865d1caea9eb949b294846711
xiongxianzhu <929679459@qq.com>
2018-07-18 11:37:10 +0800

    update

--- qingmi/utils/__init__.py
+++ qingmi/utils/__init__.py
@@ -4,8 +4,9 @@ from .json_msg import success, error, json_success, json_error
 from .md5 import md5
 from .dateformat import today, parse_datetime
 from .helper import get_uid
+from .random import random_index
 
 __all__ = [
     'success', 'error', 'json_success', 'json_error', 'md5',
-    'today', 'parse_datetime', 'get_uid'
+    'today', 'parse_datetime', 'get_uid', 'random_index'
 ]
---
+++ qingmi/utils/random.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+def random_index(rate):
+    """ 随机变量的概率函数 """
+    #
+    # 参数rate为list<int>
+    # 返回概率事件的下标索引
+    start = 0
+    index = 0
+    randnum = random.randint(0, sum(rate))
+
+    for index, scope in enumerate(rate):
+        start += scope
+        if randnum < start:
+            break
+    return index
+
+# def random_index(rate):
+#     """ 随机变量的概率函数 """
+#     #
+#     # 参数rate为list<int>
+#     # 返回概率事件的下标索引
+#     start = 0
+#     index = 0
+#     randnum = random.randint(1, sum(rate))
+
+#     for index, scope in enumerate(rate):
+#         start += scope
+#         if randnum <= start:
+#             break
+#     return index
