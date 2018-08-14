3861960f6e480f857d90e9197c9d602ce2d1a6e2
xiongxianzhu <929679459@qq.com>
2018-08-14 16:15:44 +0800

    update

--- qingmi/cert/idcard.py
+++ qingmi/cert/idcard.py
@@ -9,11 +9,10 @@ def idcardcert(appcode, card_no):
     """ 身份证实名认证身份证二要素一致性验证 """
     host = 'http://idquery.market.alicloudapi.com'
     path = '/idcard/query'
-    method = 'GET'
-    # appcode = '895fbdc6bfb44a88aaa33acccf3fa322'
+    # method = 'GET'
     appcode = appcode
     querys = 'number=%s' % card_no
-    bodys = {}
+    # bodys = {}
     url = host + path + '?' + querys
     try:
         request = urllib.request.Request(url)
@@ -25,21 +24,3 @@ def idcardcert(appcode, card_no):
         return content
     except:
         return None
-
-# def idcardcert_v2(card_no, real_name):
-#     """ 身份证实名认证身份证二要素一致性验证 """
-#     host = 'http://idquery.market.alicloudapi.com'
-#     path = '/idcard/query'
-#     method = 'GET'
-#     appcode = '895fbdc6bfb44a88aaa33acccf3fa322'
-#     querys = 'number=%s' % idcard
-#     bodys = {}
-#     url = host + path + '?' + querys
-
-#     request = urllib.request.Request(url)
-#     request.add_header('Authorization', 'APPCODE ' + appcode)
-#     response = urllib.request.urlopen(request)
-#     content = response.read()
-#     if content:
-#         return content.decode("unicode-escape")
-#     return content
