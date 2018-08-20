7064ec766323b54f90850294532e08ee20bb26d2
xiongxianzhu <929679459@qq.com>
2018-08-20 11:06:40 +0800

    update

--- qingmi/model/models.py
+++ qingmi/model/models.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from datetime import datetime
-from ..base import db, cache
+from qingmi.base import db, cache
 from qingmi.utils import today
 
 
--- qingmi/utils/time.py
+++ qingmi/utils/time.py
@@ -1,2 +1,14 @@
 # coding: utf-8
 
+
+def time2seconds(t):
+    """ 时间转秒数 """
+    h, m, s = t.strip().split(":")
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+
+def seconds2time(sec):
+    """ 秒数转时间 """
+    m, s = divmod(sec, 60)
+    h, m = divmod(m, 60)
+    return "%02d:%02d:%02d" % (h, m, s)
