f4f3f9cfe851691a322bbf4f95aa9f7e3db5124d
xiongxianzhu <929679459@qq.com>
2018-09-05 15:25:32 +0800

    update

--- qingmi/utils/__init__.py
+++ qingmi/utils/__init__.py
@@ -5,11 +5,9 @@ from .md5 import md5
 from .dateformat import today, yesterday, tomorrow, parse_datetime
 from .helper import get_uid
 from .random import random_index
-from .http import is_safe_url
 
 __all__ = [
     'success', 'error', 'json_success', 'json_error', 'md5',
     'today', 'yesterday', 'tomorrow', 'parse_datetime',
-    'get_uid', 'random_index', 'is_safe_url', 'get_redirect_target',
-    'redirect_back',
+    'get_uid', 'random_index',
 ]
