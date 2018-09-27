a3297d6a875ef8f7c5a29e7a945518d056709a5b
xiongxianzhu <929679459@qq.com>
2018-09-27 11:42:07 +0800

    update

--- qingmi/stats.py
+++ qingmi/stats.py
@@ -5,6 +5,7 @@
 
 import time
 from datetime import datetime, timedelta
+from flask import request
 from qingmi.model import StatsLog
 
 
@@ -14,8 +15,8 @@ def get_date_ranger(date_start, date_end):
     :param date_end
     """
     dates = []
-    start = datetime.strftime(date_start, '%Y-%m-%d')
-    end = datetime.strftime(date_end, '%Y-%m-%d')
+    start = datetime.strptime(date_start, '%Y-%m-%d')
+    end = datetime.strptime(date_end, '%Y-%m-%d')
     size = (end - start).days
 
     if size > 0:
@@ -63,7 +64,100 @@ def get_dates(stats=True, start_key='start', end_key='end', start='', end=''):
     return start, end
 
 
-class Stats(object):
+def get_value_list(key, days, uid=None):
+    query = dict(key=key, day__in=days)
+    if uid:
+        query['uid'] = uid
+
+    length = len(days)
+    value_list = [0 for i in range(length)]
+    items = StatsLog.objects(**query)
+    for item in items:
+        value_list[days.index(item.day)] = item.value
+    return value_list
+
+
+def get_hour_list(key, day, uid=None, hour=23):
+
+    query = dict(key=key, day=day)
+    if uid:
+        query['uid'] = uid
+
+    items = StatsLog.objects(**query)
+    values = [0 for i in range(hour + 1)]
+    for item in items:
+        if item.hour <= hour:
+            values[item.hour] = item.value
+    return values
+
+
+def hour_value_list(day, key, *args, **kwargs):
+    return get_hour_list(key, day, **kwargs)
+
+
+def get_value(value, value2, default=True):
+    """ 获得比值 """
+    if value == 0:
+        return 0
+    if value2 == 0 or not value2:
+        result = value * 100.0 / abs(value)
+    else:
+        result = value * 100.0 / value2
+
+    if result < -20 and default:
+        return -20
+    elif result > 120 and default:
+        return 120
+    return result
+
+
+def get_sum_value(data):
+    """ 多个值打包成一个 """
+    values = zip
+    for v in data:
+        values = functools.partial(values, v)
+    return list(map(lambda x: sum(x), values()))
+
+
+def date_value(key, days):
+    """ 获取一个key的值或多个key的和 """
+    if isinstance(key, list):
+        value_list = [get_value_list('date_%s' % x, days) for x in key]
+        return get_sum_value(value_list)
+    return get_value_list('date_%s' % key, days)
+
+
+def hour_value(key, day):
+    """ 获取一个key的值或多个key的和 """
+    if isinstance(key, list):
+        value_list = [get_hour_list('hour_%s' % x, day) for x in key]
+        return get_sum_value(value_list)
+    return get_hour_list('hour_%s' % key, day)
+
+
+def change_value_list(data, key, days):
+    style = data.get('style', '/')
+    key_data = date_value(data.get('key'), days)
+    key2_data = date_value(data.get('key2'), days)
+    if style == '+':
+        return list(map(lambda x: x[0] + x[1], zip(key_data, key2_data)))
+    if style == '-':
+        return list(map(lambda x: x[0] - x[1], zip(key_data, key2_data)))
+    return list(map(lambda x: get_value(x[0], x[1], data.get('default', True)), zip(key_data, key2_data)))
+
+
+def hour_change_value_list(data, day, key, *args, **kwargs):
+    style = data.get('style', '/')
+    key_data = hour_value(data.get('key'), day)
+    key2_data = hour_value(data.get('key2'), day)
+    if style == '+':
+        return list(map(lambda x: x[0] + x[1], zip(key_data, key2_data)))
+    if style == '-':
+        return list(map(lambda x: x[0] - x[1], zip(key_data, key2_data)))
+    return list(map(lambda x: get_value(x[0], x[1], data.get('default', True)), zip(key_data, key2_data)))
+
+
+class StatsHelper(object):
     """ 统计助手 """
 
     def __init__(self):
@@ -104,11 +198,11 @@ class Stats(object):
                 upsert=True,
             )
 
-    def save(self， key, value, day, start, end, hour=0, field='created_at', **kwargs):
+    def save(self, key, value, day, start, end, hour=0, field='created_at', **kwargs):
         if field is not None:
             kwargs.setdefault('%s__gte' % field, start)
             kwargs.setdefault('%s__lt' % field, end)
-        self._save(key, value=value, day, hour, **kwargs)
+        self._save(key, value, day, hour, **kwargs)
 
     def stats(self, key, model, query=lambda x: x.count(), handle=lambda x: x, **kwargs):
         self.items.append(dict(
